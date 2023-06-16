9c434e19b5e0616a96685db676b109f4e496029c
Vladimír Ulman <ulman@fi.muni.cz>
2023-06-16 14:35:57 +0200

    ADD: SciviewBridge connects to SciView and does some testing key strokes

--- src/main/java/org/mastodon/mamut/SciviewBridge.java
+++ src/main/java/org/mastodon/mamut/SciviewBridge.java
@@ -1,15 +1,104 @@
 //TODO add license text
 package org.mastodon.mamut;
 
+import graphics.scenery.Sphere;
+import graphics.scenery.controls.InputHandler;
 import mpicbg.spim.data.SpimDataException;
 import org.mastodon.mamut.project.MamutProjectIO;
+import org.mastodon.mamut.util.SphereNodes;
 import org.scijava.Context;
+import org.scijava.ui.behaviour.Behaviour;
+import org.scijava.ui.behaviour.ClickBehaviour;
 import sc.iview.SciView;
 
 import java.io.IOException;
 
 public class SciviewBridge {
 
+	//data source stuff
+	final WindowManager mastodonWin;
+
+	//data sink stuff
+	final SciView sciviewWin;
+	final SphereNodes sphereNodes;
+
+	public SciviewBridge(final WindowManager mastodonMainWindow,
+	                     final SciView targetSciviewWindow)
+	{
+		this.mastodonWin = mastodonMainWindow;
+		this.sciviewWin = targetSciviewWindow;
+
+		//add the "root" node for this Mastodon session
+		Sphere parentNode = sciviewWin.addSphere();
+		//todo: make the parent node (sphere) invisible
+		parentNode.setName( mastodonMainWindow.projectManager.getProject().getProjectRoot().toString() );
+
+		//add the sciview-side displaying handler for the spots
+		this.sphereNodes = new SphereNodes(this.sciviewWin, parentNode);
+
+		//todo: add similar handler for the volume
+	}
+
+	public MamutViewBdv openSyncedBDV() {
+		final MamutViewBdv bdvWin = mastodonWin.createBigDataViewer();
+		bdvWin.getFrame().setTitle("BDV linked to Sciview");
+
+		//bdvWin.getViewerPanelMamut().renderTransformListeners().
+		//drawSpotsFromThisTimepoint(10);
+		//sciviewWin.getCamera().setfo
+
+		//temporary drawing
+		sphereNodes.showTheseSpots(mastodonWin.getAppModel(), 10);
+		keyHandlersForTestingForNow(bdvWin);
+
+		//todo: setup a callback that
+		// -- get's triggered when the BDV window is closed
+		// -- takes DE-register handlers created above
+		return bdvWin;
+	}
+
+	private void keyHandlersForTestingForNow(final MamutViewBdv forThisBdv) {
+		//handlers
+		final Behaviour clk_DEC_SPH = (ClickBehaviour) (x, y) -> sphereNodes.decreaseSphereScale();
+		final Behaviour clk_INC_SPH = (ClickBehaviour) (x, y) -> sphereNodes.increaseSphereScale();
+
+		int[] currentTimepoint = {10};
+		final Behaviour clk_PREV_TP = (ClickBehaviour) (x, y) -> {
+			currentTimepoint[0]--;
+			sphereNodes.showTheseSpots(mastodonWin.getAppModel(), currentTimepoint[0]);
+		};
+		final Behaviour clk_NEXT_TP = (ClickBehaviour) (x, y) -> {
+			currentTimepoint[0]++;
+			sphereNodes.showTheseSpots(mastodonWin.getAppModel(), currentTimepoint[0]);
+		};
+
+		//register them
+		final InputHandler handler = sciviewWin.getSceneryInputHandler();
+		handler.addKeyBinding("decrease_initial_spheres_size", "O");
+		handler.addBehaviour("decrease_initial_spheres_size", clk_DEC_SPH);
+		handler.addKeyBinding("increase_initial_spheres_size", "shift O");
+		handler.addBehaviour("increase_initial_spheres_size", clk_INC_SPH);
+
+		handler.addKeyBinding("bbaacckk","I");
+		handler.addBehaviour("bbaacckk", clk_PREV_TP);
+		handler.addKeyBinding("ffoorrww","U");
+		handler.addBehaviour("ffoorrww", clk_NEXT_TP);
+
+		//deregister them when they are due
+		forThisBdv.onClose(() -> {
+			handler.removeKeyBinding("decrease_initial_spheres_size");
+			handler.removeBehaviour("decrease_initial_spheres_size");
+			handler.removeKeyBinding("increase_initial_spheres_size");
+			handler.removeBehaviour("increase_initial_spheres_size");
+
+			handler.removeKeyBinding("bbaacckk");
+			handler.removeBehaviour("bbaacckk");
+			handler.removeKeyBinding("ffoorrww");
+			handler.removeBehaviour("ffoorrww");
+		});
+	}
+
+	// --------------------------------------------------------------------------
 	static WindowManager giveMeSomeMastodon(final Context scijavaCtx)
 	throws IOException, SpimDataException {
 		String projectPath = "/home/ulman/Mette/e1/E1_reduced.mastodon";
@@ -47,8 +136,12 @@ public class SciviewBridge {
 	public static void main(String[] args) {
 		try {
 			SciView sv = createSciview();
+			sv.toggleSidebar();
 			WindowManager mastodon = giveMeSomeMastodon(sv.getScijavaContext());
-			mastodon.createBigDataViewer();
+
+			final SciviewBridge bridge = new SciviewBridge(mastodon, sv);
+			bridge.openSyncedBDV();
+
 		} catch (Exception e) {
 			System.out.println("got exception: "+e.getMessage());
 		}
