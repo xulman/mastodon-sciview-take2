5ea09e9f057fa5b9176f3f9b954092dbfa522327
Vladimír Ulman <ulman@fi.muni.cz>
2023-07-24 14:45:52 +0200

    FIX: adjusted the demo code to show off the AdjustableSliderControls

--- src/test/java/org/mastodon/mamut/TestingAdjustableSlider.java
+++ src/test/java/org/mastodon/mamut/TestingAdjustableSlider.java
@@ -1,13 +1,11 @@
 package org.mastodon.mamut;
 
 import org.mastodon.mamut.util.AdjustableSliderControls;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class TestingAdjustableSlider {
-
-	void demo() {
+	public static void main(String[] args) {
 		JFrame frame = new JFrame("slider test");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -15,47 +13,57 @@ public class TestingAdjustableSlider {
 		frame.setLayout( gridBagLayout );
 
 		final GridBagConstraints c = new GridBagConstraints();
-		c.anchor = GridBagConstraints.LINE_START;
-		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.3;
+		c.fill = GridBagConstraints.BOTH;
 
-
-		//set to the current wanted range
-		JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 33);
-
-		//set limits to the data type (here, GRAY16)
-		SpinnerNumberModel minModel = new SpinnerNumberModel(slider.getMinimum(), 0,65535, 50);
-		SpinnerNumberModel maxModel = new SpinnerNumberModel(slider.getMaximum(), 0,65535, 50);
-		JSpinner minSpinner = new JSpinner(minModel);
-		JSpinner maxSpinner = new JSpinner(maxModel);
-		//
+		//row
 		c.gridy = 0;
-		c.weightx = 0.05;
 		c.gridx = 0;
-		frame.add(minSpinner, c);
-		c.weightx = 0.9;
+		c.weightx = 0.1;
+		frame.add( new JButton("no op"), c);
 		c.gridx = 1;
-		frame.add(slider, c);
-		c.weightx = 0.05;
+		c.weightx = 0.8;
+		frame.add( new JButton("no op"), c);
 		c.gridx = 2;
-		frame.add(maxSpinner, c);
+		c.weightx = 0.1;
+		frame.add( new JButton("no op"), c);
 
-		AdjustableSliderControls ctrl = new AdjustableSliderControls(slider,minSpinner,maxSpinner);
+		//row
+		c.weightx = 0;
+		c.gridy = 1;
+		c.gridx = 0;
+		frame.add( new JButton("no op"), c);
+		//x=1 is missing for now
+		c.gridx = 2;
+		frame.add( new JButton("no op"), c);
 
-		JLabel msg = new JLabel("Current slider value: "+slider.getValue());
-		c.gridx=1;
-		c.gridy=1;
+		//row
+		c.gridy = 2;
+		c.gridx = 0;
+		frame.add( new JButton("no op"), c);
+		//x=1 is missing for now
+		c.gridx = 2;
+		frame.add( new JButton("no op"), c);
+
+		final JPanel placeHolder = new JPanel();
+		c.gridy = 1;
+		c.gridx = 1;
+		frame.add(placeHolder, c);
+		//
+		AdjustableSliderControls slider
+				= AdjustableSliderControls.createAndPlaceHere(placeHolder, 33, 0,100);
+
+		final JLabel msg = new JLabel("Current slider value: "+slider.getValue());
+		c.gridy = 2;
+		c.gridx = 1;
 		frame.add(msg, c);
-		ctrl.addChangeListener(l -> {
+		//
+		slider.addChangeListener(l -> {
 			msg.setText("Current slider value: "+slider.getValue());
 			System.out.print('.');
 		});
 
 		frame.pack();
 		frame.setVisible(true);
-	}
-
-
-	public static void main(String[] args) {
-		new TestingAdjustableSlider().demo();
 	}
 }
