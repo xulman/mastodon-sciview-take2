be5590e6384f014ec69568a74a9986558d773e3f
Vladimír Ulman <ulman@fi.muni.cz>
2023-07-26 14:38:47 +0200

    CHG: SciviewPlugin switched from text to GUI info panel

--- src/main/java/org/mastodon/mamut/plugins/SciviewPlugin.java
+++ src/main/java/org/mastodon/mamut/plugins/SciviewPlugin.java
@@ -2,11 +2,12 @@ package org.mastodon.mamut.plugins;
 
 import org.mastodon.mamut.SciviewBridge;
 import org.scijava.command.Command;
-import org.scijava.log.LogService;
-import org.scijava.plugin.Menu;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-
+import org.scijava.plugin.Menu;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
 import static sc.iview.commands.MenuWeights.HELP;
 import static sc.iview.commands.MenuWeights.HELP_HELP;
 
@@ -15,23 +16,60 @@ import static sc.iview.commands.MenuWeights.HELP_HELP;
 				@Menu(label = "Mastodon Bridge", weight = HELP_HELP) })
 public class SciviewPlugin implements Command
 {
-	@Parameter
-	LogService logService;
-
 	@Override
 	public void run()
 	{
-		StringBuilder msg = new StringBuilder("Mastodon-sciview Bridge Help:\n");
-		msg.append(SciviewBridge.key_DEC_SPH).append(" does ").append(SciviewBridge.desc_DEC_SPH).append("\n");
-		msg.append(SciviewBridge.key_INC_SPH).append(" does ").append(SciviewBridge.desc_INC_SPH).append("\n");
-		msg.append(SciviewBridge.key_COLORING).append(" does ").append(SciviewBridge.desc_COLORING).append("\n");
-		msg.append(SciviewBridge.key_CLRNG_AUTO).append(" does ").append(SciviewBridge.desc_CLRNG_AUTO).append("\n");
-		msg.append(SciviewBridge.key_CLRNG_ONOFF).append(" does ").append(SciviewBridge.desc_CLRNG_ONOFF).append("\n");
-		msg.append(SciviewBridge.key_CTRL_WIN).append(" does ").append(SciviewBridge.desc_CTRL_WIN).append("\n");
-		msg.append(SciviewBridge.key_CTRL_INFO).append(" does ").append(SciviewBridge.desc_CTRL_INFO).append("\n");
+		final JFrame panel = new JFrame("Mastodon-sciview Bridge Keys Overview");
+		panel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		//abusing the warn level to make sure the log window pops up
-		//and the help message is not left unnoticed
-		logService.warn(msg.toString());
+		final GridBagLayout gridBagLayout = new GridBagLayout();
+		panel.setLayout( gridBagLayout );
+
+		final GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5,8,0,8);
+		c.weighty = 0.1;
+		c.gridy = 0;
+		JLabel spacer = new JLabel("   ");
+		collectRowsOfText().forEach(r -> {
+			c.anchor = GridBagConstraints.EAST;
+			c.weightx = 0.3;
+			c.gridx = 0;
+			panel.add(new JLabel(r[0]), c);
+
+			c.weightx = 0.1;
+			c.gridx = 1;
+			panel.add(spacer, c);
+
+			c.anchor = GridBagConstraints.LINE_START;
+			c.weightx = 0.3;
+			c.gridx = 2;
+			panel.add(new JLabel(r[1]), c);
+			c.gridy++;
+		});
+
+		JButton closeBtn = new JButton("Close");
+		closeBtn.addActionListener(l -> panel.dispose());
+		c.gridx = 0;
+		c.gridwidth = 3;
+		c.weightx = 0;
+		c.weighty = 0;
+		c.insets = new Insets(15,30,5,30);
+		panel.add(closeBtn, c);
+
+		panel.pack();
+		panel.setVisible(true);
+	}
+
+	private Collection<String[]> collectRowsOfText() {
+		Collection<String[]> rows = new ArrayList<>(15);
+		rows.add(new String[] {SciviewBridge.key_DEC_SPH, SciviewBridge.desc_DEC_SPH});
+		rows.add(new String[] {SciviewBridge.key_INC_SPH, SciviewBridge.desc_INC_SPH});
+		rows.add(new String[] {SciviewBridge.key_COLORING, SciviewBridge.desc_COLORING});
+		rows.add(new String[] {SciviewBridge.key_CLRNG_AUTO, SciviewBridge.desc_CLRNG_AUTO});
+		rows.add(new String[] {SciviewBridge.key_CLRNG_ONOFF, SciviewBridge.desc_CLRNG_ONOFF});
+		rows.add(new String[] {SciviewBridge.key_CTRL_WIN, SciviewBridge.desc_CTRL_WIN});
+		rows.add(new String[] {SciviewBridge.key_CTRL_INFO, SciviewBridge.desc_CTRL_INFO});
+		return rows;
 	}
 }
