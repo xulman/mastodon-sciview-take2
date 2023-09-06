7252f854caf343aae302cb4527ebf5ccdf8e6805
Vladimír Ulman <ulman@fi.muni.cz>
2023-09-06 12:11:58 +0200

    CHG,TST: StartMastodon now opens the Mastodon Launcher...
    ...using which one can open/load a project and start sciview from
    the project - just like one would do it in the real life setting

--- src/test/java/org/mastodon/mamut/StartMastodon.java
+++ src/test/java/org/mastodon/mamut/StartMastodon.java
@@ -1,15 +1,27 @@
 package org.mastodon.mamut;
 
 import net.imagej.ImageJ;
+import org.mastodon.mamut.launcher.MastodonLauncher;
+import sc.iview.SciView;
 
 public class StartMastodon {
 	public static void main(String[] args) {
 		try {
+			//gives sciview/scenery a chance to run at all...
+			SciView.xinitThreads();
+
+			//without this, the GUI harvesting of plugins will not happen
 			ImageJ ij = new ImageJ();
 			ij.ui().showUI();
-			//NB: otherwise GUI harvesting of plugins will not happen
 
-			StartSciviewBridgeDirectly.giveMeSomeMastodon(ij.context());
+			//cannot use this one as it opens its own context...
+			//new MastodonLauncherCommand().run();
+			//
+			//...thus copy&paste the run() above.. this time with the shared context
+			final MastodonLauncher launcher = new MastodonLauncher( ij.getContext() );
+			launcher.setLocationByPlatform( true );
+			launcher.setLocationRelativeTo( null );
+			launcher.setVisible( true );
 		} catch (Exception e) {
 			System.out.println("Got this exception: "+e.getMessage());
 		}
