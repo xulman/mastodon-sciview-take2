e3a52f231e0fe174f860398f566c6322e2aea702
Samuel Pantze <83579186+smlpt@users.noreply.github.com>
2024-05-21 13:17:30 +0200

    SphereLinkNodes: unify instancing and updating into showInstancedSpots

--- src/main/kotlin/util/SphereLinkNodes.kt
+++ src/main/kotlin/util/SphereLinkNodes.kt
@@ -20,7 +20,6 @@ import sc.iview.SciView
 import sc.iview.event.NodeChangedEvent
 import java.util.*
 import kotlin.math.sqrt
-import kotlin.time.TimeSource
 
 
 //TODO FAILED to hook up here a 'parentNode' listener that would setVisible(false) on all children
@@ -40,112 +39,67 @@ class SphereLinkNodes
         events = sv.scijavaContext?.getService(EventService::class.java)
     }
 
-    /** Data class that combines the spatio-temporal index and the corresponding instance. */
-    data class IndexedSpotInstance(val spot: Spot, val instance: InstancedNode.Instance)
+    /** Data class that combines the mastodon spot, the corresponding instance and its time-point. */
+    data class IndexedSpotInstance(val spot: Spot, val instance: InstancedNode.Instance, val tp: Int)
 
     val sphere = Icosphere(1f, 2)
     lateinit var mainInstance: InstancedNode
     lateinit var spots: SpatialIndex<Spot>
 
-    /** Initializes the main spot instance, publishes it to the scene and populates it with instances from time-point 0. */
-    fun initializeInstancedSpots(
+    /** Shows or initializes the main spot instance, publishes it to the scene and populates it with instances from the current time-point. */
+    fun showInstancedSpots(
         mastodonData: ProjectModel,
         timepoint: Int,
-        colorizer: GraphColorGenerator<Spot, Link>
+        colorizer: GraphColorGenerator<Spot, Link>,
+        initializing: Boolean = false
     ) {
-        logger.debug("Initializing Spots")
-        sphere.setMaterial(ShaderMaterial.fromFiles("DeferredInstancedColor.vert", "DeferredInstancedColor.frag")) {
-            diffuse = Vector3f(1.0f, 1.0f, 1.0f)
-            ambient = Vector3f(1.0f, 1.0f, 1.0f)
-            specular = Vector3f(.0f, 1.0f, 1.0f)
-            metallic = 0.0f
-            roughness = 1.0f
+
+        if (initializing) {
+            logger.debug("Initializing Spots")
+            sphere.setMaterial(ShaderMaterial.fromFiles("DeferredInstancedColor.vert", "DeferredInstancedColor.frag")) {
+                diffuse = Vector3f(1.0f, 1.0f, 1.0f)
+                ambient = Vector3f(1.0f, 1.0f, 1.0f)
+                specular = Vector3f(.0f, 1.0f, 1.0f)
+                metallic = 0.0f
+                roughness = 1.0f
+            }
+
+            mainInstance = InstancedNode(sphere)
+            // Instanced properties should be aligned to 4*32bit boundaries, hence the use of Vector4f instead of Vector3f here
+            mainInstance.instancedProperties["Color"] = { Vector4f(1f) }
+            sv.addNode(mainInstance, parent = parentNode)
         }
-        mainInstance = InstancedNode(sphere)
-        // Instanced properties should be aligned to 4*32bit boundaries, hence the use of Vector4f instead of Vector3f here
-        mainInstance.instancedProperties["Color"] = { Vector4f(1f) }
+
         if (spotRef == null) spotRef = mastodonData.model.graph.vertexRef()
         val focusedSpotRef = mastodonData.focusModel.getFocusedVertex(spotRef)
         spots = mastodonData.model.spatioTemporalIndex.getSpatialIndex(timepoint)
         sv.blockOnNewNodes = false
 
-        sv.addNode(mainInstance, parent = parentNode)
         val covArray = Array(3) { DoubleArray(3) }
         var cov: Covariance
         var inst: InstancedNode.Instance
+
         for (s in spots) {
             inst = mainInstance.addInstance()
-            inst.name = "${s.internalPoolIndex}"
+            inst.name = "${timepoint}_${s.internalPoolIndex}"
             inst.addAttribute(Material::class.java, sphere.material())
             // add the instance to spotList, connected with the corresponding spatio-temporal index
-            spotList.add(IndexedSpotInstance(s, inst))
+            spotList.add(IndexedSpotInstance(s, inst, timepoint))
             s.localize(spotPosition)
             s.getCovariance(covArray)
             cov = Covariance(covArray)
             if (s.internalPoolIndex%5 == 0) {
                 logger.info("covariance for spot ${s.internalPoolIndex} is ${cov.covarianceMatrix}")
             }
-            inst.spatial {
-                position = Vector3f(spotPosition)
-            }
-            inst.spatial().scale = Vector3f(
-                SCALE_FACTOR * sqrt(s.boundingSphereRadiusSquared).toFloat()
-            )
+            inst.spatial().position = Vector3f(spotPosition)
+            inst.spatial().scale = Vector3f(SCALE_FACTOR * sqrt(s.boundingSphereRadiusSquared).toFloat())
             setInstancedSphereColor(inst, colorizer, s,false)
             inst.parent = parentNode
-            logger.info("initialized spot ${s.internalPoolIndex} at pos ${inst.spatial().position}")
-        }
-    }
-
-    fun updateInstancedSpots(
-        mastodonData: ProjectModel,
-        timepoint: Int,
-        colorizer: GraphColorGenerator<Spot, Link>
-    ) {
-        spots = mastodonData.model.spatioTemporalIndex.getSpatialIndex(timepoint)
-        logger.info("updating: got ${spots.size()} spots for timepoint $timepoint")
-        for (s in spots) {
-            s.localize(spotPosition)
-            val existingSpot = spotList.find { it.spot == s }
-//            logger.info("found spot ${s.internalPoolIndex}: ${existingSpot != null}")
-            if (existingSpot != null) {
-                logger.info("found spot ${s.internalPoolIndex} in existing spots")
-                // update existing spot
-                existingSpot.instance.visible = true
-                existingSpot.instance.spatial {
-                    position = Vector3f(spotPosition)
-                }
-                logger.info("updated existing spot ${s.internalPoolIndex} to pos ${existingSpot.instance.spatial().position}")
-            } else {
-                logger.info("added spot ${s.internalPoolIndex} to list because it didn't exist")
-                // create a new spot if none exists in spotList yet
-                val inst = mainInstance.addInstance()
-                inst.addAttribute(Material::class.java, sphere.material())
-                spotList.add(IndexedSpotInstance(s, inst))
-                inst.spatial {
-                    position = Vector3f(spotPosition)
-                }
-                setInstancedSphereColor(inst, colorizer, s,false)
-                inst.parent = parentNode
-                logger.info("added new spot ${s.internalPoolIndex} at pos ${inst.spatial().position}")
-            }
         }
 
-        // disable all left-over spots that exist in spotList but not in the current time-point
-//        val spotsToDisable = spotList.filter { inst ->
-//            !spots.any { spot -> spot == inst.spot }
-//        }
         for (s in spotList) {
-            val existingSpot = spots.find { it == s.spot }
-            if (existingSpot == null) {
-                s.instance.visible = false
-            }
+            s.instance.visible = s.tp == timepoint
         }
-//        logger.info("disabled ${spotsToDisable.size} spots")
-
-//        for (s in spotsToDisable) {
-//            s.instance.visible = false
-//        }
     }
 
     fun showTheseSpots(
